cc0e46abbec79875336a2539cfd6e5149d3b8212
Christopher <chrishen@hku.hk>
2018-07-06 15:28:28 +0800

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-go-ipfs-arm32v7
+# go-ipfs docker image builder for arm32v7
+
+Compatible with arm32v7 architectural, including all series of raspberry pi.
+
+Source build code: https://github.com/ipfs/go-ipfs
+
+docker hub: https://hub.docker.com/r/yrzr/go-ipfs-arm32v7
---
+++ check-version.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SEARCH_PAGES='1'
+
+if [ -f ./version_list ]; then
+    rm version_list
+fi
+
+for i in $(seq 1 ${SEARCH_PAGES}); do
+   curl -s "https://github.com/ipfs/go-ipfs/releases" | grep "ipfs/go-ipfs/releases/tag" | grep -v '\-rc' | sed 's/.*tag\/\(.*\)\">/\1/' | sort -u >> version_list;
+done
+sort -ru version_list -o version_list
