ae1b687baa1d1248d2a7a8a9aeb7ae1aeafa695f
Christopher <chrishen@hku.hk>
2018-07-06 16:24:46 +0800

    Update check-version.sh

--- check-version.sh
+++ check-version.sh
@@ -6,7 +6,5 @@ if [ -f ./version_list ]; then
     rm version_list
 fi
 
-for i in $(seq 1 ${SEARCH_PAGES}); do
-   curl -s "https://github.com/ipfs/go-ipfs/releases" | grep "ipfs/go-ipfs/releases/tag" | grep -v '\-rc' | sed 's/.*tag\/\(.*\)\">/\1/' | sort -u >> version_list;
-done
+curl -s "https://github.com/ipfs/go-ipfs/releases" | grep "ipfs/go-ipfs/releases/tag" | grep -v '\-rc' | sed 's/.*tag\/\(.*\)\">/\1/' | sort -u >> version_list
 sort -ru version_list -o version_list
